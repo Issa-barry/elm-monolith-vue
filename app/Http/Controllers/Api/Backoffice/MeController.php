3e66bd50642a0f573f9384ee9d0076e348d42949
Issa-barry <issabarry67@gmail.com>
2026-06-09 16:32:41 +0200

    gty

---
+++ app/Http/Controllers/Api/Backoffice/MeController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\Backoffice;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class MeController extends Controller
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        $user = $request->user();
+
+        $site = $user->sites()
+            ->wherePivot('is_default', true)
+            ->first()
+            ?? $user->sites()->first();
+
+        return response()->json([
+            'id'         => $user->id,
+            'prenom'     => $user->prenom,
+            'nom'        => $user->nom,
+            'telephone'  => $user->telephone,
+            'email'      => $user->email,
+            'roles'      => $user->getRoleNames(),
+            'is_active'  => $user->is_active,
+            'qr_payload' => $user->id,
+            'site'       => $site ? [
+                'id'   => $site->id,
+                'nom'  => $site->nom,
+                'code' => $site->code,
+                'ville' => $site->ville,
+            ] : null,
+        ]);
+    }
+}
