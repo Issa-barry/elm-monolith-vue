ff3aedb743b5940beb7db3eac2d0da366d1344b0
Issa-barry <issabarry67@gmail.com>
2026-07-18 17:28:42 +0200

    style: use single quotes in lang/fr/passwords.php
    Fixes Pint CI failure on PR #307 (single_quote rule).

--- lang/fr/passwords.php
+++ lang/fr/passwords.php
@@ -5,5 +5,5 @@ return [
     'sent' => 'Le lien de réinitialisation du mot de passe a été envoyé par e-mail.',
     'throttled' => 'Veuillez patienter avant de réessayer.',
     'token' => 'Ce jeton de réinitialisation est invalide.',
-    'user' => "Aucun utilisateur trouvé avec cette adresse e-mail.",
+    'user' => 'Aucun utilisateur trouvé avec cette adresse e-mail.',
 ];
