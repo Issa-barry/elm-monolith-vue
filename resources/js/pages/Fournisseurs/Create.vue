f246c9bc2b098b16a3cd2293728fda999573c98f
Issa-barry <issabarry67@gmail.com>
2026-08-10 20:16:14 +0200

    maj

--- resources/js/pages/Fournisseurs/Create.vue
+++ resources/js/pages/Fournisseurs/Create.vue
@@ -107,9 +107,7 @@ watch(
                 <Spinner v-if="form.processing" class="h-4 w-4" />
                 <Save v-else class="h-4 w-4" />
                 {{
-                    form.processing
-                        ? 'Enregistrement…'
-                        : 'Créer le fournisseur'
+                    form.processing ? 'Enregistrement…' : 'Créer le fournisseur'
                 }}
             </button>
         </div>
